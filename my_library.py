ccf586e61f8399bbbc2ada3b174250893dd370fa
fickas <fickas@cs.uoregon.edu>
2020-12-01 12:40:17 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -16,7 +16,7 @@ path  =  '/content' # "/path/to/store/your/cloned/project"
 clone = "git clone gitolite@<server_ip>:/your/project/name.git" 
 
 #os.system("sshpass -p your_password ssh user_name@your_localhost")
-os.chdir(path) # Specifying the path where the cloned project needs to be copied
+#os.chdir(path) # Specifying the path where the cloned project needs to be copied
 os.system(clone_url) # Cloning
 
 import uo_puddles.uo_puddles as up
