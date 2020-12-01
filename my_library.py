1dcc50069a479e8af90180d500388f958ddff2e1
fickas <fickas@cs.uoregon.edu>
2020-12-01 15:15:36 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -4,21 +4,19 @@ import sys
 import os
 import math
 
+#bring in spacy. Note might want to comment this out if don't like wait time.
 os.system('python -m spacy download en_core_web_md')
 import en_core_web_md
 nlp = en_core_web_md.load()
-def nlp_test(s):
-      return nlp(s)
-  
+
+#bring in uo-puddles
 my_github_name = 'uo-puddles'
 my_library_name = 'uo_puddles'
 clone_url = f'git clone https://github.com/{my_github_name}/{my_library_name}.git'  #create the url to get the library
 os.system(clone_url) # Cloning
-
 import uo_puddles.uo_puddles as up
 
+#put your functions below
 def hello():
   print('hello')
-  
-def dead_week(number):
-  return number
+
